ba1bbaf0d4b038d1177399e7413792c356ff4584
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2024-01-15 10:45:20 +0100

    Skapa AuthorDto i applikations projekt.

---
+++ Application/Dtos/AuthorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Dtos
+{
+    public class AuthorDto
+    {
+        public Guid AuthorId { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+    }
+}
