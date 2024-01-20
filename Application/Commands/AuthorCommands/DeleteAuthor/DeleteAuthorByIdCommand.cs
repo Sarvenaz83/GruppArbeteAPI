ac4061169fb3ee8c0abc03d6c500dcea59d946af
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2024-01-20 13:43:36 +0100

    Commit all changes to project.

---
+++ Application/Commands/AuthorCommands/DeleteAuthor/DeleteAuthorByIdCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Application.Commands.AuthorCommands.DeleteAuthor
+{
+    public class DeleteAuthorByIdCommand : IRequest<bool>
+    {
+        public Guid AuthorId { get; set; }
+
+        public DeleteAuthorByIdCommand(Guid authorId)
+        {
+            AuthorId = authorId;
+        }
+    }
+}
---
+++ Application/Commands/AuthorCommands/DeleteAuthor/DeleteAuthorByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Repository.AuthorRepository;
+using MediatR;
+
+namespace Application.Commands.AuthorCommands.DeleteAuthor
+{
+    public class DeleteAuthorByIdCommandHandler : IRequestHandler<DeleteAuthorByIdCommand, bool>
+    {
+
+        private readonly IAuthorRepository _authorRepository;
+
+        public DeleteAuthorByIdCommandHandler(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<bool> Handle(DeleteAuthorByIdCommand request, CancellationToken cancellationToken)
+        {
+            var authorToDelete = await _authorRepository.GetAuthorByIdAsync(request.AuthorId);
+            if (authorToDelete == null)
+            {
+                return false;
+            }
+
+            await _authorRepository.DeleteAuthorByIdAsync(request.AuthorId);
+            return true;
+        }
+    }
+}
