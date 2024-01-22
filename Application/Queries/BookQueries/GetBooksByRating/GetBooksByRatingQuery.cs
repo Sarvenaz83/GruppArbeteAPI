9942cc26f9b00a6ece87ec04f2f8e784eefd9a17
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2024-01-22 22:58:49 +0100

    Den delen kommer att lägga till Get books by rating in book contorller och alla delar som bör hänga med. Testen är klat också.

---
+++ Application/Queries/BookQueries/GetBooksByRating/GetBooksByRatingQuery.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Queries.BookQueries.GetBooksByRating
+{
+    public class GetBooksByRatingQuery : IRequest<List<Book>>
+    {
+        public decimal MinimumRating { get; set; }
+        public GetBooksByRatingQuery(decimal minimumRating)
+        {
+            MinimumRating = minimumRating;
+        }
+    }
+}
