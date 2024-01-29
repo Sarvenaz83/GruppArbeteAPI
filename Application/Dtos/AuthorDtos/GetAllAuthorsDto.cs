5400acefe2cb36d8dc6059c25e95a6eecbe97175
cjarkemar <carljohan.arkemar@gmail.com>
2024-01-29 21:20:55 +0100

    clean respons(GetAllAuthors+CreateAuthor)

--- Application/Dtos/AuthorDtos/GetAllAuthorsDto.cs
+++ Application/Dtos/AuthorDtos/GetAllAuthorsDto.cs
@@ -2,8 +2,8 @@
 using System;
 namespace Application.Dtos.AuthorDtos
 {
-	public class GetAllAuthorsDto
-	{
+    public class GetAllAuthorsDto
+    {
         public Guid AuthorId { get; set; }
 
         public string? AuthorName { get; set; }
